5199e53768c120b0dc01314f7fd7c2057dfd57aa
hewzCode <hewzcode@gmail.com>
2024-04-28 15:48:33 -0500

    commit3

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ export default function Home() {
       
         <h1>This is the body h1</h1>
         <p>This is the body p</p>
+        <h1 className=" "></h1>
         
     <main>
       <h1>niggas</h1>
